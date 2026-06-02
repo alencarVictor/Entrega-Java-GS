aac510fcd4fe8d7861b9a686667e05252660ae7f
Victor Alencar <alencarfelkipe@gmail.com>
2026-06-01 22:58:50 -0300

    Implementa classe Piloto

--- src/beans/Piloto.java
+++ src/beans/Piloto.java
@@ -1,4 +1,4 @@
-package bean;
+package beans;
 
 public class Piloto extends Tripulante {
 
@@ -6,4 +6,33 @@ public class Piloto extends Tripulante {
     private double horasDeVoo;
     private String licenca;
 
+    //Constutor Vazio
+    public Piloto() {}
+
+
+    //Construtor Cheio
+    public Piloto(String nome, String especialidade, int matricula, double horasDeVoo, String licenca) {
+        super(nome, especialidade, matricula);
+        this.horasDeVoo = horasDeVoo;
+        this.licenca = licenca;
+    }
+
+    //Getters e Setters
+    public double getHorasDeVoo() {return horasDeVoo;}
+    public void setHorasDeVoo(double horasDeVoo) {this.horasDeVoo = horasDeVoo;}
+
+    public String getLicenca() {return licenca;}
+    public void setLicenca(String licenca) {this.licenca = licenca;}
+
+    //ToString
+    public String toString(){
+        return super.toString() +
+                ", horasDeVoo=" + horasDeVoo +
+                ", licenca='" + licenca + '\'';
+    }
+
+    //Metodos da Classe
+    public void registrarHorasVoo(double horas) {
+        this.horasDeVoo += horas;
+    }
 }
