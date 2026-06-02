f459a930d6e8bfe76f09a0bb753fc83ea0afaa8f
Victor Alencar <alencarfelkipe@gmail.com>
2026-06-01 22:52:17 -0300

    feat: Implementa classe Tripulante

--- src/beans/Tripulante.java
+++ src/beans/Tripulante.java
@@ -1,4 +1,4 @@
-package bean;
+package beans;
 
 public class Tripulante {
 
@@ -6,4 +6,33 @@ public class Tripulante {
     private String nome;
     private String especialidade;
     private int matricula;
+
+    //Construtor vazio
+    public Tripulante() {}
+
+    //Construtor Cheio
+    public Tripulante(String nome, String especialidade, int matricula) {
+        this.nome = nome;
+        this.especialidade = especialidade;
+        this.matricula = matricula;
+    }
+
+    //Getters e Setters
+    public String getNome() {return nome;}
+    public void setNome(String nome) {this.nome = nome;}
+
+    public String getEspecialidade() {return especialidade;}
+    public void setEspecialidade(String especialidade) {this.especialidade = especialidade;}
+
+    public int getMatricula() {return matricula;}
+    public void setMatricula(int matricula) {this.matricula = matricula;}
+
+    //ToString
+    public String toString(){
+        return "Tripulante{" +
+                "nome=" + nome +
+                ", especialidade=" + especialidade +
+                ", matricula=" + matricula +
+                '}';
+    }
 }
